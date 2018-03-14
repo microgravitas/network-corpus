1ef2f87f204fccce1034e1c24cd01fd85ba523aa
ubik <reidl@cs.rwth-aachen.de>
2018-03-14 18:00:10 +0000

    Domset stats

--- statistics/sparsity.py
+++ statistics/sparsity.py
@@ -20,6 +20,10 @@ def compute_dtf4(graph, filename, logger, timeout=None) -> min:
  	"""		Computes the max indegree of a 4-augmentation.  	"""
  	return dtf(filename, 4, logger, None)  	
 
+def compute_dtf5(graph, filename, logger, timeout=None) -> min:
+ 	"""		Computes the max indegree of a 5-augmentation.  	"""
+ 	return dtf(filename, 5, logger, None)   	
+
 def dtf(filename, r, logger, timeout=None) -> min:
  	"""
 		Computes the max indegree of an r-augmentation.
@@ -30,3 +34,34 @@ def dtf(filename, r, logger, timeout=None) -> min:
  	with Rhizome.from_file(filename) as rhizome:
  		rhizome.augment(r);
  		return rhizome.max_in_degree() 		
+
+def compute_domset(graph, filename, logger, timeout=None) -> min:
+ 	"""		Computes the size of an (approximate) dominating set.  	"""
+ 	return dtf(filename, 1, logger, None)
+
+def compute_domset2(graph, filename, logger, timeout=None) -> min:
+ 	"""		Computes the size of an (approximate) 2-dominating set.  	"""
+ 	return dtf(filename, 2, logger, None)
+
+def compute_domset3(graph, filename, logger, timeout=None) -> min:
+ 	"""		Computes the size of an (approximate) 2-dominating set.  	"""
+ 	return dtf(filename, 3, logger, None) 	
+
+def compute_domset4(graph, filename, logger, timeout=None) -> min:
+ 	"""		Computes the size of an (approximate) 2-dominating set.  	"""
+ 	return dtf(filename, 4, logger, None) 	
+
+def compute_domset5(graph, filename, logger, timeout=None) -> min:
+ 	"""		Computes the size of an (approximate) 2-dominating set.  	"""
+ 	return dtf(filename, 5, logger, None) 	 	
+
+def domset(filename, r, logger, timeout=None) -> min:
+ 	"""
+		Computes an approximate r-domset.
+ 	"""
+ 	from .dtf.rhizome import Rhizome
+ 	import sys
+ 	
+ 	with Rhizome.from_file(filename) as rhizome:
+ 		ds = rhizome.domset(r);
+ 		return len(ds)
